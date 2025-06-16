f3a1e09cd0732685d9fd0409786ea63c7aa81583
Turbo-Nishant <turbo@Turbo.localdomain>
2025-06-17 02:47:37 +0530

    bug fix for running condition

--- app/controllers/item_controller.py
+++ app/controllers/item_controller.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
-from app.services.item_service import add_item, get_items 
+from app.services.item_service import add_item, get_all_items_service 
 
 # Controller to list all items
-async def get_items():
+async def list_items():
     # Await the coroutine from the service layer
-    return await get_items()
+    return await get_all_items_service()
--- app/services/item_service.py
+++ app/services/item_service.py
@@ -1,11 +1,11 @@
 from app.dto.item import ItemCreate, ItemResponse
-from app.persistence.item import add_item, get_item, get_items
+from app.persistence.item import add_item_to_db, get_item, get_items
 
 async def add_item(itemCreate: ItemCreate) -> ItemResponse:
-    db_item = await add_item(itemCreate)
+    db_item = await add_item_to_db(itemCreate)
     return ItemResponse.from_orm(db_item)
 
 
-async def get_items():
+async def get_all_items_service():
     db_items = await get_items()
     return [ItemResponse.from_orm(db_item) for db_item in db_items]
